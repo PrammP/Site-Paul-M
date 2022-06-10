6eb2bd18d8fa4c663662d861974fc08cb363d012
PrammP <92103872+PrammP@users.noreply.github.com>
2022-06-10 20:40:37 +0200

    sql

--- src/php/traitement.php
+++ src/php/traitement.php
@@ -6,25 +6,25 @@
     $pwd='';
 
         try{
-            $newBD= new PDO('mysql:host=$host;port=$port;dbname=$dbname',$user,$pwd);
+            $newBD= new PDO("mysql:host=$host;port=$port;dbname=$dbname",$user,$pwd);
             print "Connexion etablie";
         }
         catch(PDOException $e){
             die('Erreur :'.$e->getMessage());
         }
 
-        if (isset($_POST['nom'])&&
-           isset($_POST['prenom'])&&
-           isset($_POST['email'])&&
-           isset($_POST['cgu'])&&
-           isset($_POST['comment'])
+        var_dump($_POST);
+
+        if (isset($_POST["nom"])&&
+           isset($_POST["prenom"])&&
+           isset($_POST["email"])&&
+           isset($_POST["cgu"])
            ){
-               $insertion=$newBD->prepare('INSERT INTO profil VALUES(NULL,:nom,:prenom,:email,:cgu,:comment)');
-               $insertion->bindValue(':nom',$_POST['nom']);
-               $insertion->bindValue(':prenom',$_POST['prenom']);
-               $insertion->bindValue(':email',$_POST['email']);
-               $insertion->bindValue(':cgu',$_POST['cgu']);
-               $insertion->bindValue(':comment',$_POST['comment']);
+               $insertion=$newBD->prepare("INSERT INTO profil VALUES(NULL,:nom,:prenom,:email,:comment)");
+               $insertion->bindValue(":nom",$_POST["nom"]);
+               $insertion->bindValue(":prenom",$_POST["prenom"]);
+               $insertion->bindValue(":email",$_POST["email"]);
+               $insertion->bindValue(":comment",$_POST["comment"]);
                $verification=$insertion->execute();
                if ($verification) {
                    print "<br>Données Envoyé !";
@@ -34,7 +34,7 @@
                }
            }
            else{
-               print "Une variable n'est pas declaree et ou null";
+               print "<br>Une variable n'est pas declaree et ou null";
            }
            
         
